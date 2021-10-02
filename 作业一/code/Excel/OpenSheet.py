665ccceff0bebe62f7daee8389bdab7a1f947c5e
zeekcheung <zeekcheung@gmail.com>
2021-10-03 02:19:32 +0800

    打开Excel和修改单元格内容

---
+++ 作业一/code/Excel/OpenSheet.py
@@ -0,0 +1,4 @@
+from openpyxl import load_workbook
+
+wb = load_workbook('作业1_数据预处理数据集')
+ws = wb['航空公司客户数据']
---
+++ 数据预处理/Excel/EditSheet.py
@@ -0,0 +1,34 @@
+import math
+
+from Excel import OpenSheet
+
+wb = OpenSheet.wb
+ws = OpenSheet.ws
+
+max_row = 2002
+max_col = 7
+
+for col in ws.iter_cols(min_col=3, max_col=7):
+	position = []
+	dataSum = 0  # 数字维度的和
+	dataAvg = 0  # 数字维度的平均值
+	for cell in col:
+		val = cell.value
+		# 终止条件
+		if val == None:
+			break
+		# 跳过表头
+		if isinstance(val, str) and val != '?':
+			continue
+		if val == '?':
+			position.append(cell.column)
+		else:
+			dataSum += val
+
+	dataAvg = math.floor(dataSum / (max_row - 2 - len(position)))
+
+	for row in position:
+		ws.cell(row=row, column=col[0].column).value = dataAvg
+
+wb.save('数据清理后的数据集.xlsx')
+wb.close()
